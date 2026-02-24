b138fd75dafe9b1056464980b801ca503d377af2
bvrajan90-design <bvrajan90@gmail.com>
2026-02-25 00:26:34 +0530

    Add Metro configuration for React Native project

---
+++ metro.config.js
@@ -0,0 +1,13 @@
+const { getDefaultConfig } = require('expo/metro-config');
+const { mergeConfig } = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = getDefaultConfig(__dirname);
+
+module.exports = mergeConfig(config, {});
+
